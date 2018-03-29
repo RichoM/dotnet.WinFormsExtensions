9b3ac851fecf3dc393b58f163244cdac24ae9a07
RichoM <richi.moran@gmail.com>
2018-03-29 18:46:45 -0300

    Added InputBox.

---
+++ src/WinFormsExtensions/WinFormsExtensions/InputBox.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace System.Windows.Forms
+{
+    partial class InputBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.acceptButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputTextBox.Location = new System.Drawing.Point(13, 13);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(292, 20);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // acceptButton
+            // 
+            this.acceptButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.acceptButton.Location = new System.Drawing.Point(230, 39);
+            this.acceptButton.Name = "acceptButton";
+            this.acceptButton.Size = new System.Drawing.Size(75, 23);
+            this.acceptButton.TabIndex = 1;
+            this.acceptButton.Text = "Accept";
+            this.acceptButton.UseVisualStyleBackColor = true;
+            this.acceptButton.Click += new System.EventHandler(this.acceptButton_Click);
+            // 
+            // InputBox
+            // 
+            this.AcceptButton = this.acceptButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 68);
+            this.Controls.Add(this.acceptButton);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "InputBox";
+            this.Text = "InputBox";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button acceptButton;
+    }
+}
---
+++ src/WinFormsExtensions/WinFormsExtensions/InputBox.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Windows.Forms
+{
+    public partial class InputBox : Form
+    {
+        public static string Ask(string title = "InputBox", string defaultValue = "")
+        {
+            InputBox form = new InputBox();
+            form.Text = title;
+            form.Value = defaultValue;
+            form.ShowDialog();
+            return form.Value;
+        }
+        
+        private InputBox()
+        {
+            InitializeComponent();
+        }
+
+        public string Value
+        {
+            get { return inputTextBox.Text; }
+            set { inputTextBox.Text = value; }
+        }
+
+        private void acceptButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
